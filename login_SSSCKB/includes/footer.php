fadf3c712dd18bf4d5677cf40bce5aea2f574332
scpstar <70367344+scpstar@users.noreply.github.com>
2021-11-04 21:08:55 +0930

    Delete login_SSSCKB directory

--- login_SSSCKB/includes/footer.php
+++
@@ -1,4 +0,0 @@
-        <div class="container">
-            <!--adding footer to the page and when clicking on it, it should go to home page-->
-            <p class="text-center">&copy; <a href='/<?php echo $root_dir ?>/index.php'>SSSC Knowledge Base</a> 2021</p>
-        </div> 
